7b9c85cec5dfa63eb500794a40d5eb3f67a08585
Mel DeJesus <meldejesus22@gmail.com>
2017-04-09 17:23:38 -0400

    finished

--- timeSheetLogic3.js
+++ timeSheetLogic3.js
@@ -15,12 +15,12 @@ var name;
 var destination;
 var firstArrival;
 var frequency;
-var movies=[];
+var movies = [];
 var markup;
 var currentTime;
-  var timeDifference; 
-  var addTime;
-var interval;  
+var timeDifference;
+var addTime;
+var interval;
 var firstTime;
 var currentTime;
 var diffTime;
@@ -29,39 +29,31 @@ var tMinutes;
 var nextTrain;
 // Read 
 
-var d = new Date(); // for now
-console.log(d);
-var h=d.getHours(); // => 9
-var m=d.getMinutes(); // =>  30
-var s=d.getSeconds(); // => 51
-
-
 database.ref().limitToLast(5).on("child_added", function(snapshot) {
 
   //make sure that there's something in the database if you're going to read it 
   var exists = snapshot.exists();
   if (exists) {
     var data = snapshot.val();
-    movies.push(data);   
+    movies.push(data);
 
-  for (i=0; i<movies.length; i++){
-//assign html markup along with the variables, which are then dynamically inserted into the html
-      markup = "<tr><td>"+
-      movies[i].name+
-      "</td><td>" + 
-      movies[i].destination + 
-      "</td><td>" + 
-      movies[i].frequency + 
-      "</td></tr>"+
-      movies[i].nextTrain + 
-      "</td></tr>+"
-       movies[i].tMinutes + 
-      "</td></tr>+"
-      ;
-      }
-//#data-table is an empty <tbody> tag that is filled in
-      $("#data-table").append(markup);
-}
+    for (i = 0; i < movies.length; i++) {
+      //assign html markup along with the variables, which are then dynamically inserted into the html
+      markup = "<tr><td>" +
+        movies[i].name +
+        "</td><td>" +
+        movies[i].destination +
+        "</td><td>" +
+        movies[i].frequency +
+        "</td><td>" +
+        movies[i].nextTrain +
+        "</td><td>" +
+        movies[i].tMinutes +
+        "</td><td>";
+    }
+    //#data-table is an empty <tbody> tag that is filled in
+    $("#data-table").append(markup);
+  }
 });
 
 /// WRITE
@@ -77,29 +69,27 @@ $("#submit-btn").on("click", function(event) {
   frequency = $("#frequency").val().trim();
   frequency = parseInt(frequency);
 
-  firstTime=moment(firstArrival, "hh:mm").subtract(1, "years");
+  firstTime = moment(firstArrival, "hh:mm").subtract(1, "years");
   console.log(moment(firstTime).format("hh:mm"));
-   currentTime=moment();
-   console.log("moment: " + moment(currentTime));
-   diffTime=moment().diff(moment(firstTime), "minutes");
-   console.log("difference: " + diffTime);
-   tRemainder=diffTime%frequency;
-   console.log("remainder: " + tRemainder);
-   tMinutes=frequency-tRemainder;
-   console.log("tMinutes: " + tMinutes);
-   nextTrain=moment().add(tMinutes, "minutes");
-   console.log("next Train  " + moment(nextTrain).format("hh:mm"));
+  currentTime = moment();
+  console.log("moment: " + moment(currentTime));
+  diffTime = moment().diff(moment(firstTime), "minutes");
+  console.log("difference: " + diffTime);
+  tRemainder = diffTime % frequency;
+  console.log("remainder: " + tRemainder);
+  tMinutes = frequency - tRemainder;
+  console.log("tMinutes: " + tMinutes);
+  nextTrain = moment().add(tMinutes, "minutes");
+  console.log("next Train  " + moment(nextTrain).format("hh:mm"));
 
-  
   database.ref().push({
-    
+
     name: name,
     destination: destination,
     nextTrain: moment(nextTrain).format("hh:mm"),
     frequency: frequency,
     tMinutes: tMinutes
 
-
-  });  
+  });
 
 });
